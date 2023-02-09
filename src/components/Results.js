cd227656dcdd20199714cd1b88c2f8c5ad9faafb
Soizic Venel <venel.soizic@hotmail.fr>
2023-02-09 14:29:55 +0100

    add margin to card component

--- src/components/Results.js
+++ src/components/Results.js
@@ -10,7 +10,7 @@ const Results = props => {
         <>
             <p><img src={picture} alt='winner'/></p>
             <h2 className='results'>Results</h2>
-            {props.nbResponses === 0 && <p className='resultsText'>That's a shame, try again!</p>}
+            {props.nbResponses === 0 && <p className='resultsText'>No good answer... That's a shame, try again!</p>}
             {props.nbResponses === 1 && <p className='resultsText'>You got <span className='number'>{props.nbResponses}</span> correct answer! <br/>You can do better, try again!</p>}
             {props.nbResponses > 1 && <p className='resultsText'>You got <span className='number'>{props.nbResponses}</span> correct answers!</p>}
             {props.nbResponses >= 10 && <p className='resultsText'>You got <span className='number'>{props.nbResponses}</span> correct answers!<br/>You're on fire! Can you do better?</p>}
